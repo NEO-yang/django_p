92c8cef9c38071ebc46eb361bc7524fad7db9986
Neo <1214795754@qq..com>
2024-06-18 18:08:45 +0800

    2-4

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    birth = models.DateField(blank=True, null=True)
+    phone = models.CharField( max_length=50,null=True)
+    phonee = models.CharField( max_length=50,null=True)
+
+
+    def __str__(self):
+        return 'user {}'.format(self.user.username)
 
 # Create your models here.
