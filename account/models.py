68bc68c30ceb7a7cde11e1fa0f94484597dcfe51
home <1214795754@qq.com>
2024-06-19 23:17:38 +0800

    2-6-3

--- account/models.py
+++ account/models.py
@@ -12,3 +12,13 @@ class UserProfile(models.Model):
         return 'user {}'.format(self.user.username)
 
 # Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True) 
+    school = models.CharField(max_length=100, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    profession = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    aboutme = models.TextField(blank=True)
+    
+    def __str__(self):
+        return "user:{}".format(self.user.username)
