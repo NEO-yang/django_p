da6f55d20fceb8e63d219fa02e3fe22106b2bf80
Neo <1214795754@qq..com>
2024-07-09 18:10:52 +0800

    4-6

--- article/forms.py
+++ article/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import ArticleColumn, ArticlePost
+from .models import ArticleColumn, ArticlePost, Comment
 class ArticleColumnForm(forms.ModelForm):
     class Meta:
         model = ArticleColumn
@@ -9,3 +9,9 @@ class ArticlePostForm(forms.ModelForm):
     class Meta:
         model = ArticlePost 
         fields = ("title", "body")
+
+
+class CommentForm(forms.ModelForm): 
+    class Meta:
+        model = Comment
+        fields = ("commentator", "body",)
